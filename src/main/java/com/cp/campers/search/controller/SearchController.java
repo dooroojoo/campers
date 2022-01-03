a702dd8d16a07533fd6de6c27739ea3cd0b0338b
dooroojoo <dooroojoo@kakao.com>
2022-01-03 17:39:16 +0900

    헤더 a태그 css 수정, 메인페이지 controller 수정

--- src/main/java/com/cp/campers/search/controller/SearchController.java
+++ src/main/java/com/cp/campers/search/controller/SearchController.java
@@ -2,15 +2,26 @@ package com.cp.campers.search.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@RequestMapping("/search")
 public class SearchController {
 	
-	@GetMapping("/searchCamp")
+	@GetMapping("camp")
 	public String searchCamp() {
 		return "search/searchCamp";
 	}
 	
+	@GetMapping("main")
+	public String mainSearch(@RequestParam("area") String area,
+							 @RequestParam("daterange") String date,
+							 @RequestParam("guest") int guest
+							) {
+		
+		return "search/searchCamp";
+	}
 
 }
