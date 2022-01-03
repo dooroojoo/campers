016b9472ac04381cd1b4174f99d9c641681e4930
dooroojoo <dooroojoo@kakao.com>
2022-01-03 20:04:22 +0900

    캠핑장 검색 로직 수정, application.yml ignore설정

---
+++ src/main/java/com/cp/campers/search/model/service/SearchService.java
@@ -0,0 +1,15 @@
+package com.cp.campers.search.model.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.cp.campers.search.model.vo.SearchCamp;
+
+
+public interface SearchService {
+	
+	/* 메인페이지에서 캠핑장 검색 조회 */
+	List<SearchCamp> mainSearch(HashMap<String, Object> map);
+	
+
+}
