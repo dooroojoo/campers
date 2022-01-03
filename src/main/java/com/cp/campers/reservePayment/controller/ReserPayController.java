e44c7cc853aa94c17c531bb368711c8d9af0657b
Jeongonhwa <onhwav4432@gmail.com>
2022-01-04 00:42:53 +0900

    예약 insert 중간 커밋

--- src/main/java/com/cp/campers/reservePayment/controller/ReserPayController.java
+++ src/main/java/com/cp/campers/reservePayment/controller/ReserPayController.java
@@ -1,14 +1,38 @@
 package com.cp.campers.reservePayment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cp.campers.reservePayment.model.ReserPayService;
 
 @Controller
+@RequestMapping("/reserPay")
 public class ReserPayController {
 
+	ReserPayService reserPayService;
 	
-	@GetMapping("/reserPay")
+	@Autowired
+	public ReserPayController(ReserPayService reserPayService) {
+		this.reserPayService = reserPayService;
+	}
+	
+	
+//	@GetMapping("/reserPay")
+//	public String reserPay() {
+//		return "reservePayment/reserPay";
+//	} 
+	
 	public String reserPay() {
 		return "reservePayment/reserPay";
+	}
+	
+	
+	@PostMapping("/insert")
+	public String insertReserPay(//) {
+		//reserPayService.insertReserPay();
+		
+		return null;
 	}
 }
---
+++ src/main/java/com/cp/campers/reservePayment/model/ReserPayService.java
@@ -0,0 +1,5 @@
+package com.cp.campers.reservePayment.model;
+
+public interface ReserPayService {
+
+}
