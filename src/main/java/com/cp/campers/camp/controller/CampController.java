88b5be67ba915a4865b9956b81a60cd1d3f79cdc
boming09 <lace23@naver.com>
2022-01-06 13:45:53 +0900

    숙소 상세

--- src/main/java/com/cp/campers/camp/controller/CampController.java
+++ src/main/java/com/cp/campers/camp/controller/CampController.java
@@ -1,7 +1,10 @@
 package com.cp.campers.camp.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,8 +22,12 @@ public class CampController {
 	}
 
 	@GetMapping("detail")
-	public String campDetail(int campNo) {
+	public String campDetail(int campNo, Model model) {
 		
+		Map<String, Object> map = campService.campDetail(campNo);
+		model.addAttribute("camp", map.get("camp"));
+		model.addAttribute("roomList", map.get("roomList"));
+		model.addAttribute("newReply", '\n');
 		
 		return "camp/campDetail";
 	}
--- src/main/java/com/cp/campers/camp/model/dao/CampMapper.java
+++ src/main/java/com/cp/campers/camp/model/dao/CampMapper.java
@@ -1,8 +1,19 @@
 package com.cp.campers.camp.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.cp.campers.camp.model.vo.Camp;
+import com.cp.campers.camp.model.vo.Room;
 
 @Mapper
 public interface CampMapper {
+
+	// 숙소 조회
+	Camp campDetail(int campNo);
+
+	// 객실 조회
+	List<Room> detailRoom(int campNo);
 
 }
