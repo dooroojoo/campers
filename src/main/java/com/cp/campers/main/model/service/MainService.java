34774da4f54e6d081fb36b9c600801263c37e54b
dooroojoo <dooroojoo@kakao.com>
2022-01-08 00:18:30 +0900

    main페이지 db 연결 후 view 수정

--- src/main/java/com/cp/campers/main/model/service/MainService.java
+++ src/main/java/com/cp/campers/main/model/service/MainService.java
@@ -9,7 +9,11 @@ public interface MainService {
 	// 메인페이지 슬라이드 추천 리스트 조회
 	List<Recommend> mainSlider();
 
-	
+	// 메인페이지 예약순 3개 리스트 조회
+	List<Recommend> mainBestList();
+
+	// 메인페이지 신규순 3개 리스트 조회
+	List<Recommend> mainNewList();
 	
 	
 }
