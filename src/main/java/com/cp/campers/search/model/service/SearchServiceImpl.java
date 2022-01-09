389866b883ca61e46f68adabfa5f21bbe3072222
dooroojoo <dooroojoo@kakao.com>
2022-01-09 18:09:38 +0900

    캠핑장 전체 리스트 페이징 처리 수정

--- src/main/java/com/cp/campers/search/model/service/SearchServiceImpl.java
+++ src/main/java/com/cp/campers/search/model/service/SearchServiceImpl.java
@@ -2,6 +2,7 @@ package com.cp.campers.search.model.service;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,8 +28,31 @@ public class SearchServiceImpl implements SearchService{
 	}
 
 	@Override
-	public List<SearchCamp> campAllSearch(PageInfo pi) {
-		return searchMapper.campAllSearch(pi);
+	public Map<String, Object> campAllSearch(int nowPage) {
+		
+		// 페이지 전체 개수 조회
+		int listCount = searchMapper.campListCount();
+		
+		// 2. PageInfo 객체 만들기
+		PageInfo pi = new PageInfo(nowPage, listCount, 10, 5);
+		pi.setStartRow(nowPage, pi.getCampLimit());
+		pi.setEndRow(pi.getStartRow(), pi.getCampLimit());
+		
+		// 3. 페이징 처리 된 게시글 목록 조회
+		List<SearchCamp> campAllList = searchMapper.campAllSearch(pi);
+		
+		Map<String, Object> returnMap = new HashMap<>();
+		
+		if(listCount == 0) {
+			returnMap.put("searchSize", "검색된 결과가 없습니다.");
+		} else {
+			returnMap.put("searchSize", listCount+1+"개의 캠핑장이 조회되었습니다.");
+		}
+		
+		returnMap.put("pi", pi);
+		returnMap.put("campAllList", campAllList);
+		
+		return returnMap;
 	}
 
 	@Override
