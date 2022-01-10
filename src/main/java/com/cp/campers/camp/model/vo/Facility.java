96e776e7d43c5de2e3796d178103277d9b6412f6
boming09 <lace23@naver.com>
2022-01-10 16:49:27 +0900

    사업장형태/주요부가시설

---
+++ src/main/java/com/cp/campers/camp/model/vo/Facility.java
@@ -0,0 +1,12 @@
+package com.cp.campers.camp.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Facility {
+
+	private int fNo;		// 시설코드
+	private String fName;	// 시설명
+	private int fType;		// 시설구분
+	
+}
