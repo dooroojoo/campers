2fddd98c3968361b166f61a120673e8374c3563c
dooroojoo <dooroojoo@kakao.com>
2022-01-19 17:14:55 +0900

    예약가능여부 표시 수정

--- src/main/java/com/cp/campers/search/model/vo/SearchCamp.java
+++ src/main/java/com/cp/campers/search/model/vo/SearchCamp.java
@@ -11,7 +11,6 @@ public class SearchCamp {
 	private String cAddress;	// 숙소주소
 	private String fileRoute;	// 파일경로(FILE테이블참조)
 	private String fileName;	// 파일이름(FILE테이블참조)
-	private int rCount;			// 잔여객실수량(ROOM테이블참조)
 	private int maxGuest;		// 객실최대인원(ROOM테이블참조)
 	private int minPrice;		// 객실최소가격(ROOM테이블참조)
 	private int maxPrice;		// 객실최대가격(ROOM테이블참조)
